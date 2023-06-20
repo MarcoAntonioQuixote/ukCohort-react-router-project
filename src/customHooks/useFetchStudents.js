f470ef6709ac7e625c2620aa426155eb2ba07463
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2023-06-20 14:55:42 -0600

    testing deploy

--- src/customHooks/useFetchStudents.js
+++ src/customHooks/useFetchStudents.js
@@ -6,6 +6,8 @@ const useFetchStudents = () => {
 
     const [students,setStudents] = useState([]);
 
+    console.log(process.env.REACT_APP_API_URL);
+
     const instance = axios.create({
         baseURL: process.env.REACT_APP_API_URL,
         headers: {"content-type": "application/json"}
